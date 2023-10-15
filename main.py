06cf846e2090c2c9b257d8cfe7b1d274652ba0d3
Nathan Touchton <ntouchton@protonmail.com>
2023-10-15 18:21:28 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from os import system
+
 # https://spiritbox.com/pages/tour
 # https://architectsofficial.com/tour
+
+def curl(website:str):
+    """Runs the curl command for whatever website you pass into it."""
+    system(f"curl '{website}'")
+
+print(curl("https://ntprogramming.us"))
