95583649f28d5238897a115eb034d3d4c7c79820
Nathan Touchton <ntouchton@protonmail.com>
2023-10-15 20:14:50 -0500

    new file:   emailtemplate.py 	modified:   main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from os import system
+# from emailtemplate import SendEmail
 
 # https://spiritbox.com/pages/tour
 # https://architectsofficial.com/tour
+
+# Curl websites for HTML
+# Determine where to look for city/state names
 
 def curl(website:str):
     """Runs the curl command for whatever website you pass into it."""
     system(f"curl '{website}'")
 
-print(curl("https://ntprogramming.us"))
+# curl("https://spiritbox.com/pages/tour")
+# Spiritbox html class: bit-location-under-mobile
+
+# curl("https://architectsofficial.com/tour")
+# Architects html class: bit-location-under-mobile
+
+# Send email if city and/or state lines up with what I'm looking for
+# Prevent from sending too many emails somehow
