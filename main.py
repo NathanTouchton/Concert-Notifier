7571dabb1e4e4d83d89dd31bc584c81388801d67
Nathan Touchton <ntouchton@protonmail.com>
2023-10-16 12:21:09 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-from os import system
+# from os import system
+from requests import get
 # from emailtemplate import SendEmail
-
-# https://spiritbox.com/pages/tour
-# https://architectsofficial.com/tour
 
 # Curl websites for HTML
 # Determine where to look for city/state names
 
-def curl(website:str):
-    """Runs the curl command for whatever website you pass into it."""
-    system(f"curl '{website}'")
+# def curl(website:str):
+#     """Runs the curl command for whatever website you pass into it."""
+#     return system(f"curl '{website}'")
 
-# curl("https://spiritbox.com/pages/tour")
+spiritbox_site = "https://spiritbox.com/pages/tour"
 # Spiritbox html class: bit-location-under-mobile
 
-# curl("https://architectsofficial.com/tour")
+architects_site = "https://architectsofficial.com/tour"
 # Architects html class: bit-location-under-mobile
+
+a = get(url=architects_site)
+print(a.text)
 
 # Send email if city and/or state lines up with what I'm looking for
 # Prevent from sending too many emails somehow
