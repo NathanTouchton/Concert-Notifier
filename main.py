5f379b0b23e4bc1b43199fc30efb94702766aa97
Nathan Touchton <ntouchton@protonmail.com>
2023-10-18 14:39:53 -0500

    modified:   main.py 	new file:   requirements.txt

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-# from os import system
 from requests import get
+from bs4 import BeautifulSoup
 # from emailtemplate import SendEmail
 
-# Curl websites for HTML
+# Scrape websites for HTML
 # Determine where to look for city/state names
-
-# def curl(website:str):
-#     """Runs the curl command for whatever website you pass into it."""
-#     return system(f"curl '{website}'")
 
 spiritbox_site = "https://spiritbox.com/pages/tour"
 # Spiritbox html class: bit-location-under-mobile
@@ -15,8 +11,12 @@ spiritbox_site = "https://spiritbox.com/pages/tour"
 architects_site = "https://architectsofficial.com/tour"
 # Architects html class: bit-location-under-mobile
 
-a = get(url=architects_site)
-print(a.text)
+architects = get(url=architects_site).text
+soup = BeautifulSoup(architects, "html.parser")
+# print(soup.find("a", {"class": "bit-details"}))
+print(soup.prettify())
 
 # Send email if city and/or state lines up with what I'm looking for
 # Prevent from sending too many emails somehow
+
+# It looks like I'll need to use Selenium
